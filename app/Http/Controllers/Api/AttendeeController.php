13c1f4b4e1726bb7749dc745aa14752ddf62b424
Hongorix <kox69@yahoo.com>
2023-08-26 00:22:12 +0300

    Final commit
    added notifications about events via email and rate limits

--- app/Http/Controllers/Api/AttendeeController.php
+++ app/Http/Controllers/Api/AttendeeController.php
@@ -18,6 +18,8 @@ class AttendeeController extends Controller
     public function __construct()
     {
         $this->middleware('auth:sanctum')->except(['index', 'show', 'update']);
+        $this->middleware('throttle:api')
+            ->only(['store', 'destroy']);
         $this->authorizeResource(Attendee::class, 'attendee');
     }
 
